b1890048decc4ba6ce897585e79f486f2f005c05
unknown <kapongclub125@hotmail.com>
2016-03-22 10:10:36 +0700

    text

---
+++ app/src/main/java/appewtc/masterung/ungrestaurant/MainActivity.java
@@ -0,0 +1,62 @@
+package appewtc.masterung.ungrestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    //Explicit
+    private EditText userEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+    private String userString, passwordString;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bineWidget();
+
+        //Button Controller
+        buttonController();
+    } // Main Method นี่คือ คลาสรอง
+
+    private void buttonController() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            //Receive Value From Edit Text
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+                //check Spack
+                if (userString.equals("") || passwordString.equals("")){
+                    //Have Space
+                    MyAlertDialog myAlertDialog = new MyAlertDialog();
+                    myAlertDialog.myDialog(MainActivity.this,
+                    "มีช่องว่าง","กรุณากรอกทุกช่อง");
+                } else {
+
+                }
+
+
+
+                 //onClick
+            }
+        });
+    } // buttonController
+
+    private void bineWidget() {
+
+
+        userEditText = (EditText) findViewById(R.id.editText);
+        passwordEditText = (EditText) findViewById(R.id.editText2);
+        loginButton = (Button) findViewById(R.id.button);
+    }
+} // Main Class นี่คือ คลาสหลัก
